2b067ad4212e938353ef1894379b8aa2cfc8fa33
John McLaughlin <John10mc@gmail.com>
2020-11-05 01:05:30 +0000

    Started c version

---
+++ ImperativeVersion/BST.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int value;
+    node left;
+    node right;
+}
+
+struct tree{
+    node root;
+}
+
+struct add(node root, int value){
+    if root.
+}
+
+
+int main(){
+    printf("Hello")
+    return 0;
+}
