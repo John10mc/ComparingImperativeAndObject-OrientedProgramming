867e065e6bcbba1fefb9a77cdc0e529779a5c48f
John McLaughlin <John10mc@gmail.com>
2020-11-07 02:09:25 +0000

    Full working bst for C

--- ImperativeVersion/BST.c
+++ ImperativeVersion/BST.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct node{
     int value;
@@ -13,8 +14,7 @@ struct tree{
 
 struct node* add(struct node *root, int value){
     if(root == NULL){
-        struct node* newNode;
-        newNode = (struct node*)malloc(sizeof(struct node));
+        struct node* newNode = (struct node*)malloc(sizeof(struct node));
         newNode->value = value;
         //printf("Value: %d", newNode.value);
         return newNode;
@@ -27,6 +27,53 @@ struct node* add(struct node *root, int value){
     }
 };
 
+struct node* find(struct node *root, int value){
+    if(root->value == value){
+        return root;
+    }
+    else if(value < root->value){
+        return find(root->left, value);
+    }
+    else{
+        return find(root->right, value);
+    }
+};
+
+struct node* delete(struct node *root, int value){
+    if(root == NULL){
+        return NULL;
+    }
+    else if(value > root->value){
+        root->right = delete(root->right, value);
+    }
+    else if(value < root->value){
+        root->left = delete(root->left, value);  
+    }
+    else{
+        if(root->left == NULL && root->right == NULL){
+            return NULL;
+        }
+        else if(root->left == NULL || root->right == NULL){
+            if(root->left == NULL){
+                return root->right;
+            }
+            else{
+                return root->left;  
+            }
+        }
+        else{
+            struct node* minNode = root->right;
+            struct node* parentNode;
+            while(minNode != NULL){
+                parentNode = minNode;
+                minNode = minNode->left;
+            }
+            root->value = parentNode->value;
+            root->right = delete(root->right, parentNode->value);
+        }
+    }
+}
+
 struct node *traverse(struct node *root){
     if(root != NULL){
         //printf("Here2");
@@ -35,23 +82,49 @@ struct node *traverse(struct node *root){
         traverse(root->right);
         //printf("test2");
     }
-}
+};
 
 
 int main(){
+    //printf("here");
     int nodes[] = {57, 10, 93, 88, 77, 74, 78};
     size_t nodesLength = sizeof(nodes) / sizeof(nodes[0]);
+    char line[200];
+    char name[50];
+    char *names[5];
+    FILE *file = fopen("data.txt", "r");
 
-    struct node* root = NULL;
-    //struct node* left = NULL;
-    //struct node* right = NULL;
-
-
-    //root->left = left;
-    //root->right = right;
-
-    for(int i=0; i < nodesLength; i++){
-        root = add(root, nodes[i]);
+    int i = 0;
+    int nameIndex = 0;
+    while (fgets(line, 100, file) != NULL) {
+        while(line[i] != ','){
+            //printf("%c", line[i]);
+            name[i] = line[i];
+            i++;
+        }
+        i = 0;
+        printf("%s\n", name);
+        memset(&name[0], 0, sizeof(name));
+        strcpy(&names[nameIndex], name);
+        nameIndex++;
     }
-    traverse(root);
+
+    printf("%s", names[2]);
+    
+    //fclose(file);
+
+    // struct node* root = NULL;
+
+    // for(int i=0; i < nodesLength; i++){
+    //     root = add(root, nodes[i]);
+    // }
+    // printf("Root: %d\n", root->left->value);
+    // traverse(root);
+
+    // struct node* found = find(root, 93);
+    // printf("Found value: %d\n", found->value);
+
+
+    // delete(root, 74);
+    // traverse(root);
 }
