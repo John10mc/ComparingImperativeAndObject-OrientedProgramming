faa563d8310695cb95e709c86224bb84275239db
John McLaughlin <John10mc@gmail.com>
2020-11-07 19:02:31 +0000

    Add, find and traverse working for bst with numbers and names

--- ImperativeVersion/BST.c
+++ ImperativeVersion/BST.c
@@ -3,13 +3,16 @@
 #include <string.h>
 
 struct node{
-    struct node* left;
-    struct node *right;
-    char *value;
+    char *name;
+    char *address;
+    char *number;
 };
 
 struct tree{
-    struct node root;
+    struct node *record;
+    char *id;
+    struct tree *left;
+    struct tree *right;
 };
 
 int compareTo(char *s1, char *s2){
@@ -39,79 +42,83 @@ int compareTo(char *s1, char *s2){
     else{
         return -1;
     }
-}
-
-struct node* add(struct node *root, char *value){
-    if(root == NULL){
-        struct node *newNode = (struct node*)malloc(sizeof(struct node));
-        (newNode)->value = malloc(sizeof(value));
-        strcpy((newNode)->value, value);
-        //newNode->value = value;
-        //printf("Value: %1s", newNode->value);
-        return newNode;
-    }
-    else if(compareTo(value, root->value) == 1){
-        //printf("Left");
-        root->left = add(root->left, value);
-    }
-    else{
-        //printf("Right");
-        root->right = add(root->right, value);
-    }
 };
 
-struct node* find(struct node *root, char *value){
-    if(compareTo(value, root->value) == 0){
+struct tree* add(struct tree *root, struct node *newNode, char *value){
+    if(root == NULL){
+        struct tree *newTree = (struct tree*)malloc(sizeof(struct tree));
+        newTree->record = newNode;
+        newTree->id = value;
+
+        printf("Inserted record: %s, %s, %s\n",(newNode)->name, (newNode)->address, (newNode)->number);
+        return newTree;
+    }
+    else if(compareTo(value, root->id) == -1){
+        //printf("Left");
+        root->left = add(root->left, newNode, value);
+    }
+    else if(compareTo(value, root->id) == 1){
+        //printf("Right");
+        root->right = add(root->right, newNode, value);
+    }
+    else{
+        printf("Record: %s, %s, %s already exists in phonebook.\n",(newNode)->name, (newNode)->address, (newNode)->number);
         return root;
     }
-    else if(compareTo(value, root->value) == -1){
-        return find(root->left, value);
+};
+
+struct tree* find(struct tree *root, char *id){
+    if(compareTo(id, root->id) == 0){
+        return root;
+    }
+    else if(compareTo(id, root->id) == 1){
+        return find(root->left, id);
     }
     else{
-        return find(root->right, value);
+        return find(root->right, id);
     }
 };
 
-struct node* delete(struct node *root, char *value){
-    if(root == NULL){
-        return NULL;
-    }
-    else if(compareTo(value, root->value) == -1){
-        root->right = delete(root->right, value);
-    }
-    else if(compareTo(value, root->value) == 1){
-        root->left = delete(root->left, value);  
-    }
-    else{
-        if(root->left == NULL && root->right == NULL){
-            return NULL;
-        }
-        else if(root->left == NULL || root->right == NULL){
-            if(root->left == NULL){
-                return root->right;
-            }
-            else{
-                return root->left;  
-            }
-        }
-        else{
-            struct node* minNode = root->right;
-            struct node* parentNode;
-            while(minNode != NULL){
-                parentNode = minNode;
-                minNode = minNode->left;
-            }
-            root->value = parentNode->value;
-            root->right = delete(root->right, parentNode->value);
-        }
-    }
-}
+// struct node* delete(struct node *root, char *name){
+//     if(root == NULL){
+//         return NULL;
+//     }
+//     else if(compareTo(name, root->name) == -1){
+//         root->right = delete(root->right, name);
+//     }
+//     else if(compareTo(name, root->name) == 1){
+//         root->left = delete(root->left, name);  
+//     }
+//     else{
+//         if(root->left == NULL && root->right == NULL){
+//             return NULL;
+//         }
+//         else if(root->left == NULL || root->right == NULL){
+//             if(root->left == NULL){
+//                 return root->right;
+//             }
+//             else{
+//                 return root->left;  
+//             }
+//         }
+//         else{
+//             struct node* minNode = root->right;
+//             struct node* parentNode;
+//             while(minNode != NULL){
+//                 parentNode = minNode;
+//                 minNode = minNode->left;
+//             }
+//             root->name = parentNode->name;
+//             root->right = delete(root->right, parentNode->name);
+//         }
+//     }
+// };
 
-struct node *traverse(struct node *root){
+struct node *traverse(struct tree *root){
     if(root != NULL){
         //printf("Here2");
         traverse(root->left);
-        printf("Value: %s\n", root->value);
+        printf("Value: %s, %s, %s\n", root->record->name, root->record->address, root->record->number);
         traverse(root->right);
         //printf("test2");
     }
@@ -120,33 +127,74 @@ struct node *traverse(struct node *root){
 int main(){
     char line[200];
     char name[50];
-    struct node* root = NULL;
+    char address[100];
+    char number[50];
+    struct tree* nameTree = NULL;
+    struct tree* numberTree = NULL;
     FILE *file = fopen("data.txt", "r");
 
     int i = 0;
-    int nameIndex = 0;
+    int j = 0;
     while (fgets(line, 100, file) != NULL) {
         while(line[i] != ','){
             //printf("%c", line[i]);
-            name[i] = line[i];
+            name[j] = line[i];
             i++;
+            j++;
         }
-        root = add(root, name);
-        //printf("%s\n", name);
+        i += 2;
+        j = 0;
+        while(line[i] != ','){
+            //printf("%c", line[i]);
+            address[j] = line[i];
+            i++;
+            j++;
+        }
+        i += 2;
+        j = 0;
+        while(line[i] != '\0'){
+            //printf("%c", line[i]);
+            number[j] = line[i];
+            i++;
+            j++;
+        }
+        number[strcspn(number, "\n")] = 0;
+
+        struct node *newNode = (struct node*)malloc(sizeof(struct node));
+        (newNode)->name = malloc(sizeof(name));
+        (newNode)->address = malloc(sizeof(address));
+        (newNode)->number = malloc(sizeof(number));
+
+        strcpy((newNode)->name, name);
+        strcpy((newNode)->address, address);
+        strcpy((newNode)->number, number);
+
+        nameTree = add(nameTree, newNode, newNode->name);
+        numberTree = add(numberTree, newNode, newNode->number);
+
+        //printf(" record: %s, %s, %s\n",name, address, number);
         memset(&name[0], 0, sizeof(name));
+        memset(&address[0], 0, sizeof(address));
+        memset(&number[0], 0, sizeof(number));
         i = 0;
+        j = 0;
     }
 
-    
+    traverse(nameTree);
+    printf("\n");
+    traverse(numberTree);
     //fclose(file);
 
     //printf("Root: %s\n", root->left->value);
-    traverse(root);
+    //traverse(root);
 
-    struct node* found = find(root, "James");
-    printf("Found value: %s\n", found->value);
+    struct tree* foundName = find(nameTree, "James");
+    printf("Found record: %s, %s, %s.\n",(foundName)->record->name, (foundName)->record->address, (foundName)->record->number);
+
+    struct tree* foundNumber = find(numberTree, "508-934-1199");
+    printf("Found record: %s, %s, %s.\n",(foundNumber)->record->name, (foundNumber)->record->address, (foundNumber)->record->number);
 
 
-    delete(root, "James");
-    traverse(root);
-}
+    //delete(root, "James");
+    //traverse(root);
+};
