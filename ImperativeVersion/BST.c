b64949ddd68c5394133743926a8bf5a560baa6a3
John McLaughlin <John10mc@gmail.com>
2020-11-08 01:25:46 +0000

    Finished Imperative version

--- ImperativeVersion/BST.c
+++ ImperativeVersion/BST.c
@@ -16,10 +16,10 @@ struct tree{
     struct tree *right;
 };
 
-// struct bst{
-//     struct tree *numberTree;
-//     struct tree 
-// }
+struct binarySearchTree{
+    struct tree *numberTree;
+    struct tree *nameTree;
+};
 
 int compareTo(char *s1, char *s2){
     size_t lenS1 = strlen(s1);
@@ -65,13 +65,13 @@ struct tree* recFind(struct tree *root, char *id){
     }
 };
 
-struct tree* find(struct tree *nameTree, struct tree *numberTree, char *id){
+struct binarySearchTree* find(struct binarySearchTree *bst, char *id){
     struct tree* found;
     if(isalpha(id[0])){
-        found = recFind(nameTree, id);
+        found = recFind(bst->nameTree, id);
     }
     else{
-        found = recFind(numberTree, id);
+        found = recFind(bst->numberTree, id);
     }
     if(found != NULL){
         printf("Found record: %s, %s, %s.\n",(found)->record->name, (found)->record->address, (found)->record->number);
@@ -81,27 +81,37 @@ struct tree* find(struct tree *nameTree, struct tree *numberTree, char *id){
     }
 }
 
-struct tree* add(struct tree *root, struct node *newNode, char *value){
+struct tree* recAdd(struct tree *root, struct node *newNode, char *value){
     if(root == NULL){
         struct tree *newTree = (struct tree*)malloc(sizeof(struct tree));
         newTree->record = newNode;
         newTree->id = value;
-
         return newTree;
     }
     else if(compareTo(value, root->id) == -1){
         //printf("Left");
-        root->left = add(root->left, newNode, value);
+        root->left = recAdd(root->left, newNode, value);
     }
     else if(compareTo(value, root->id) == 1){
         //printf("Right");
-        root->right = add(root->right, newNode, value);
+        root->right = recAdd(root->right, newNode, value);
     }
-    else{
-        
+    else{     
         return root;
     }
 };
+
+struct binarySearchTree* add(struct binarySearchTree *bst, struct node *newNode){
+        if(recFind(bst->nameTree, newNode->name) == NULL){
+            bst->numberTree = recAdd(bst->numberTree, newNode, newNode->number);
+            bst->nameTree = recAdd(bst->nameTree, newNode, newNode->name);
+            printf("Inserted record: %s, %s, %s\n",(newNode)->name, (newNode)->address, (newNode)->number);
+        }
+        else{
+            printf("Record: %s, %s, %s already exists in phonebook.\n",(newNode)->name, (newNode)->address, (newNode)->number);
+        }
+    return bst;
+}
 
 struct tree* recDelete(struct tree *root, char *id){
     if(root == NULL){
@@ -136,31 +146,30 @@ struct tree* recDelete(struct tree *root, char *id){
             root->record->name = parentNode->record->name;
             root->record->address = parentNode->record->address;
             root->record->number = parentNode->record->number;
-            root->right = recDelete(root->right, parentNode->id);
+            root->right = recDelete(root->right, root->id);
         }
     }
 };
 
-struct tree* delete(struct tree *nameTree, struct tree *numberTree, char *id){
+struct binarySearchTree* delete(struct binarySearchTree *bst, char *id){
     struct tree* found;
     if(isalpha(id[0])){
-        found = recFind(nameTree, id);
+        found = recFind(bst->nameTree, id);
     }
     else{
-        found = recFind(numberTree, id);
+        found = recFind(bst->numberTree, id);
     }
     if(found != NULL){
-        recDelete(nameTree, found->record->name);
-        recDelete(numberTree, found->record->number);
         printf("Deleted record: %s, %s, %s\n", found->record->name, found->record->address, found->record->number);
+        recDelete(bst->nameTree, found->record->name);
+        recDelete(bst->numberTree, found->record->number);
     }
     else{
         printf("Could not delete record: %s. Could not be found.\n", id);
     }
-
 };
 
-struct node *traverse(struct tree *root){
+void *traverse(struct tree *root){
     if(root != NULL){
         //printf("Here2");
         traverse(root->left);
@@ -171,19 +180,22 @@ struct node *traverse(struct tree *root){
 };
 
 int main(){
+    struct binarySearchTree *bst= (struct binarySearchTree*)malloc(sizeof(struct binarySearchTree));
+    struct tree *nameTree = NULL;
+    struct tree *numberTree = NULL;
+    bst->nameTree = nameTree;
+    bst->numberTree = numberTree;
+
     char line[200];
-    char name[50];
     char address[100];
+    char name[50];
     char number[50];
-    struct tree* nameTree = NULL;
-    struct tree* numberTree = NULL;
     FILE *file = fopen("data.txt", "r");
 
     int i = 0;
     int j = 0;
-    while (fgets(line, 100, file) != NULL) {
+    while (fgets(line, 200, file) != NULL) {
         while(line[i] != ','){
-            //printf("%c", line[i]);
             name[j] = line[i];
             i++;
             j++;
@@ -191,7 +203,6 @@ int main(){
         i += 2;
         j = 0;
         while(line[i] != ','){
-            //printf("%c", line[i]);
             address[j] = line[i];
             i++;
             j++;
@@ -199,11 +210,11 @@ int main(){
         i += 2;
         j = 0;
         while(line[i] != '\0'){
-            //printf("%c", line[i]);
             number[j] = line[i];
             i++;
             j++;
         }
+
         number[strcspn(number, "\n")] = 0;
 
         struct node *newNode = (struct node*)malloc(sizeof(struct node));
@@ -214,52 +225,53 @@ int main(){
         strcpy((newNode)->name, name);
         strcpy((newNode)->address, address);
         strcpy((newNode)->number, number);
- 
-        if(recFind(nameTree, newNode->name) == NULL){
-            nameTree = add(nameTree, newNode, newNode->name);
-            numberTree = add(numberTree, newNode, newNode->number);
-            printf("Inserted record: %s, %s, %s\n",(newNode)->name, (newNode)->address, (newNode)->number);
-        }
-        else{
-            printf("Record: %s, %s, %s already exists in phonebook.\n",(newNode)->name, (newNode)->address, (newNode)->number);
-        }
+
+        add(bst, newNode);
+        //free(newNode);
 
         memset(&name[0], 0, sizeof(name));
         memset(&address[0], 0, sizeof(address));
         memset(&number[0], 0, sizeof(number));
+        memset(&line[0], 0, sizeof(line));
         i = 0;
         j = 0;
     }
 
     printf("\n");
-    traverse(nameTree);
+    traverse(bst->nameTree);
     printf("\n");
-    traverse(numberTree);
+    traverse(bst->numberTree);
     // //fclose(file);
 
     printf("\n");
-    find(nameTree, numberTree, "Max");
+    find(bst, "Max");
+    printf("\n");
+    find(bst, "James");
     printf("\n");
 
-    traverse(nameTree);
+    traverse(bst->nameTree);
     printf("\n");
-    delete(nameTree, numberTree ,"James");
+    delete(bst, "James");
     printf("\n");
-    traverse(numberTree);
+    traverse(bst->numberTree);
     printf("\n");
-    traverse(nameTree);
+    traverse(bst->nameTree);
     printf("\n");
-    delete(nameTree, numberTree ,"James");
+    delete(bst, "James");
     printf("\n");
 
-    traverse(numberTree);
+    traverse(bst->numberTree);
     printf("\n");
-    delete(nameTree, numberTree ,"530-841-5851");
+    delete(bst, "530-841-5851");
     printf("\n");
-    traverse(nameTree);
+    traverse(bst->nameTree);
     printf("\n");
-    traverse(numberTree);
+    traverse(bst->numberTree);
     printf("\n");
-    delete(nameTree, numberTree ,"530-841-5851");
+    delete(bst, "530-841-5851");
     printf("\n");
+
+    free(bst);
 };
+
+
